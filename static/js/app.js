17fa84875f31ad7123999ff956330cc0b880b00b
Matt Raley <mtraley@gmail.com>
2019-06-02 04:46:27 -0500

    submit

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,97 @@
 function buildMetadata(sample) {
-
+console.log("buildMetadata");
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
+  d3.json("http://127.0.0.1:5000/metadata/" + sample).then(function (metabacteria) {
+    console.log(metabacteria);
+    var panel = d3.select("#sample-metadata");
+    panel.html(""); // Use `.html("") to clear any existing metadata
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+    // tags for each key-value in the metadata. 
+  for (let [key,value] of Object.entries(metabacteria)) {
+      panel.append("text").text(key + ": " + value+"\n");
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    }
+  });
+
 }
 
 function buildCharts(sample) {
-
+ console.log("buildCharts");
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+ function buildPlot1() {
+    d3.json("http://127.0.0.1:5000/samples/" + sample).then(function (bacteria) {
+      console.log(bacteria);
+          var samples = bacteria.sample_values.slice(0, 10);
+          var otu_ids = bacteria.otu_ids.slice(0, 10);
+          var otu_labels = bacteria.otu_labels.slice(0, 10);
+          console.log(otu_labels);
+          var trace = {
+            labels: otu_ids,
+            values: samples,
+            type: 'pie'
 
-    // @TODO: Build a Bubble Chart using the sample data
+          };
+          var layout = {
+            title: "Belly Button Biodiversity: Pie Chart",
+            height: 525,
+            width: 525
+          };
+          var data = [trace];
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-    const left = names.slice(0, 10);
-    console.log(left);
-  
-}
+          Plotly.newPlot("pie", data, layout);
+        });
+
+
+      }
+          // @TODO: Use `d3.json` to fetch the sample data for the plots
+        function buildPlot2() {
+           d3.json("http://127.0.0.1:5000/samples/" + sample).then(function (newbacteria) {
+             console.log(newbacteria);
+                 var samples = newbacteria.sample_values.slice(0, 10);
+       
+                 var otu_ids = newbacteria.otu_ids.slice(0, 10);
+                 var otu_labels = newbacteria.otu_labels.slice(0, 10);
+                 console.log(otu_labels);
+                 var trace = {
+                   x: otu_ids,
+                   y: newbacteria.sample_values,
+                   text: otu_labels,
+                   mode: 'markers',
+                   marker:{
+                     size: samples,
+                     color: otu_ids,
+
+                   }
+                 };
+                 var data = [trace];
+                var layout = {
+                  title: "Bacteria Bubble Disribution",
+                  showlegend: true,
+                  height: 750,
+                  width: 750
+                };
+
+                Plotly.newPlot("bubble", data, layout);
+               });
+             }
+             buildPlot1();
+             buildPlot2();
+     
+      }
+
+
+    //@TODO: Build a Bubble Chart using the sample data
+
+    //@TODO: Build a Pie Chart
+    //HINT: You will need to use slice() to grab the top 10 sample_values,
+    //otu_ids, and labels (10 each).
+
+
 
 function init() {
   // Grab a reference to the dropdown select element
